fee4010c1cf351c1a8b198586bf0748e2ad8dd08
David <destrada2193@gmail.com>
2021-04-19 15:19:17 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "David Estrada";
+    int age = 28;
+    String state = "California";
+    System.out.println("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
+  }
+}
