912d952cc8479dbbaa2c39e950cee2cd561f5e3a
David <destrada2193@gmail.com>
2021-04-19 15:31:55 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 10;
+    double num2 = 15;
+    double num3 = 21;
+    double average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
