ef324265fce0e23813a77e4dda0caaafa08549bd
David <destrada2193@gmail.com>
2021-04-21 10:28:22 -0700

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 7;
+    double num2 = 10;
+    double num3 = 28;
+    double average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
