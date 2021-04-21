b5827dbef6b1e444e8ef4bc911579af69a0c2c66
David <destrada2193@gmail.com>
2021-04-21 10:45:58 -0700

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 6;
+    double side2 = 8;
+    side1 *=6;
+    side2 *=8;
+    double hypotenuse = Math.sqrt(side1 + side2);
+    System.out.println(hypotenuse);
+  }
+}
