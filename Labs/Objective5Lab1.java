186218048d214bd0d0541451783c3b1a144ad49c
David <destrada2193@gmail.com>
2021-04-26 10:55:41 -0700

    complete objective5lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,15 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+    //fill in the following logic conditions
+    if(num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+      //solution goes here
+    }
+    else if(num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1);
+      //solution goes here
+    }
+  }
+}
