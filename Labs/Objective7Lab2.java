9be4b7becba7396a313f1f05facc6b5c07e63847
David <destrada2193@gmail.com>
2021-04-27 08:59:41 -0700

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,14 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+    //print the values 0 -> 20
+      //print the value of counter with each iteration
+      //add 1 to counter each iteration so loop doesn't run forever
+
+      while(counter <= 20) {
+        System.out.println(counter);
+
+        counter = counter + 1;
+      }
+  }
+}
