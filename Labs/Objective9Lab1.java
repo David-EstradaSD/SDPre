14e0b3effe672c49624dbad0136939aa4190aeb8
David <destrada2193@gmail.com>
2021-04-27 14:31:59 -0700

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,12 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    // In this lab, we have 2 methods: main method and printName
+    // Call the provided printName method
+    printName(); // here we called the printName method
+                  // (within the main method brackets)
+    }
+
+    public static void printName() {
+      System.out.println("Hello David");
+    }
+  }
