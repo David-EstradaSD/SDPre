d076d8c73f178e7f0e7cc4d042c4c114eb940525
David <destrada2193@gmail.com>
2021-04-27 13:34:34 -0700

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String[] args){
+    //write a "for" loop that will print the numbers 1-10
+    //write another "for" loop that will print the numbers 10-1
+    for (int a = 1; a <= 10; a += 1) {
+      System.out.println(a);
+      //increments can be written as ++ or +=
+    }
+    for (int b = 10; b >= 1; b -= 1) {
+      System.out.println(b);
+      //decrements can be written as -- or -=
+    }
+  }
+}
