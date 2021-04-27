bd2cad4990182f2fa6d021b7e3962168bb2321a2
David <destrada2193@gmail.com>
2021-04-27 08:52:35 -0700

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,13 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+    //print the values 10 -> 1
+    //print the value of "counter" with each iteration
+
+    while(counter > 0) {
+      System.out.println(counter);
+
+      counter = counter - 1; //this makes us count down
+    }
+  }
+}
